76e7d86332329e340be4cd50c4954ef88da21a0f
Ander Paz <72525631+anderpaz@users.noreply.github.com>
2021-03-08 10:37:22 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h2>Exemplos de Metodos de Array!</h2>
+<div>Every: Testar se todos os elementos passam por um teste especifico</div>
+<div>Filter: retorna um array quando for true</div>
+<div>Find: retorna o primeiro elemento testado </div>
+<div>Includes: retorna true se o array, objeto possui o valor testado</div>
+<div>Map: retorna uma lista de array </div>
+<div>Reduce: retorna um valor concatenado com base no elemento de todo o array</div>
+<div>Some: Testa se pelo menos um elemento é verdadeiro, retrona boolean </div>
+<div>forEach: Executar uma função para cada item de um array sem retorno </div>
+<div> </div>
+`;
